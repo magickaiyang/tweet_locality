74dd9cad77a2f34b73e133df01594d50001f57cd
Shoto <ssinhara@purdue.edu>
2018-10-02 14:23:26 -0400

    reads file and prints number of records in file

--- find_boundary.py
+++ find_boundary.py
@@ -1,5 +1,11 @@
 from shapely.geometry import Point
+import shapefile
 
-point = Point(0.0, 0.0)
-print point.length
+myshp = open("C:/Users/Shoto/PycharmProjects/LocalityTweets/venv/shapefiles/tl_2017_us_state.dbf", "rb")
+mydbf = open("C:/Users/Shoto/PycharmProjects/LocalityTweets/venv/shapefiles/tl_2017_us_state.dbf", "rb")
+r = shapefile.Reader(shp=myshp, dbf=mydbf)
+
+print r.numRecords
+#point = Point(0.0, 0.0)
+#print point.length
 
