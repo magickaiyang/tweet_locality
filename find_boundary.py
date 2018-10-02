bc7595f622c744312a65190a45a0516e16cf030d
Shoto <ssinhara@purdue.edu>
2018-10-02 14:27:50 -0400

    prints info about every state in file

--- find_boundary.py
+++ find_boundary.py
@@ -5,7 +5,11 @@ myshp = open("C:/Users/Shoto/PycharmProjects/LocalityTweets/venv/shapefiles/tl_2
 mydbf = open("C:/Users/Shoto/PycharmProjects/LocalityTweets/venv/shapefiles/tl_2017_us_state.dbf", "rb")
 r = shapefile.Reader(shp=myshp, dbf=mydbf)
 
-print r.numRecords
-#point = Point(0.0, 0.0)
-#print point.length
+# print r.numRecords
+
+for i in range(r.numRecords):
+    print r.record(i)
+
+# point = Point(0.0, 0.0)
+# print point.length
 
