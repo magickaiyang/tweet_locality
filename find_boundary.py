9c35c0b67e35d6cccc32b1e0bf3214707df1597b
Shoto <ssinhara@purdue.edu>
2018-10-03 13:36:15 -0400

    minor changes

--- find_boundary.py
+++ find_boundary.py
@@ -1,7 +1,7 @@
 from shapely.geometry import Point
 import shapefile
 
-myshp = open("C:/Users/Shoto/PycharmProjects/LocalityTweets/venv/shapefiles/tl_2017_us_state.dbf", "rb")
+myshp = open("C:/Users/Shoto/PycharmProjects/LocalityTweets/venv/shapefiles/tl_2017_us_state.shp", "rb")
 mydbf = open("C:/Users/Shoto/PycharmProjects/LocalityTweets/venv/shapefiles/tl_2017_us_state.dbf", "rb")
 r = shapefile.Reader(shp=myshp, dbf=mydbf)
 
