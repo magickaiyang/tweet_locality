07821e1c75eea1546582f617e5bada063c471417
an82 <at82tong@gmail.com>
2018-10-19 14:44:12 -0400

    DBScan finding densest cluster

--- dbscan_test.py
+++ dbscan_test.py
@@ -1,12 +1,20 @@
+from geopy.distance import great_circle
+from shapely.geometry import MultiPoint
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.cluster import DBSCAN
 from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import numpy as np
+from collections import Counter
+import pandas as pd
 
+def get_centermost_point(cluster):
+    centroid = (MultiPoint(cluster).centroid.x, MultiPoint(cluster).centroid.y)
+    centermost_point = min(cluster, key=lambda point: great_circle(point, centroid).m)
+    return tuple(centermost_point)
 
-centers = [[1, 1], [-1, -1], [1, -1]]
+centers =np.array( [[40.430023, -86.909103], [40.422363, -86.876688], [40.422363, -86.876668], [40.425368, -86.895309], [40.366318, -86.752251]])
 X, labels_true = make_blobs(n_samples=750, centers=centers, cluster_std=0.3,
                             random_state=0)
 
@@ -14,24 +22,39 @@ X = StandardScaler().fit_transform(X)
 
 # #############################################################################
 # Compute DBSCAN
-db = DBSCAN(eps=0.3, min_samples=10).fit(X)
-core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
-core_samples_mask[db.core_sample_indices_] = True
+eps=1.5/6371.0088
+db = DBSCAN(eps=eps, min_samples=1).fit(centers)
+print(db.labels_)
+#core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+#core_samples_mask[db.core_sample_indices_] = True
 labels = db.labels_
 
 # Number of clusters in labels, ignoring noise if present.
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+c = Counter(labels)
+print(n_clusters_)
+largest_cluster = (c.most_common(1))[0][0]
+largest_cluster_index=[]
+for i in range(len(centers)):
+    if labels[i] == largest_cluster:
+        largest_cluster_index.append(centers[i])
+
+print(largest_cluster_index)
+clusters = pd.Series([centers[largest_cluster]for n in range(n_clusters_)])
+print()
+print(get_centermost_point(clusters))
+#print(labels.most_common())
 
 print('Estimated number of clusters: %d' % n_clusters_)
-print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-print("Adjusted Rand Index: %0.3f"
-      % metrics.adjusted_rand_score(labels_true, labels))
-print("Adjusted Mutual Information: %0.3f"
-      % metrics.adjusted_mutual_info_score(labels_true, labels))
-print("Silhouette Coefficient: %0.3f"
-      % metrics.silhouette_score(X, labels))
+#print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
+#print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
+#print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
+#print("Adjusted Rand Index: %0.3f"
+#      % metrics.adjusted_rand_score(labels_true, labels))
+#print("Adjusted Mutual Information: %0.3f"
+#      % metrics.adjusted_mutual_info_score(labels_true, labels))
+#print("Silhouette Coefficient: %0.3f"
+#      % metrics.silhouette_score(X, labels))
 
 # #############################################################################
 # Plot result
@@ -47,13 +70,20 @@ for k, col in zip(unique_labels, colors):
 
     class_member_mask = (labels == k)
 
-    xy = X[class_member_mask & core_samples_mask]
-    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+    xy=centers
+    plt.plot(xy, 'o', markerfacecolor=tuple(col),
              markeredgecolor='k', markersize=14)
-
-    xy = X[class_member_mask & ~core_samples_mask]
-    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+    plt.plot(xy, 'o', markerfacecolor=tuple(col),
              markeredgecolor='k', markersize=6)
+
+    # xy = X[class_member_mask & core_samples_mask]
+    # plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+    #          markeredgecolor='k', markersize=14)
+    #
+    # xy = X[class_member_mask & ~core_samples_mask]
+    # plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+    #          markeredgecolor='k', markersize=6)
 
 plt.title('Estimated number of clusters: %d' % n_clusters_)
 plt.show()
+
