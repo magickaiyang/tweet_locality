5ad58fb1154a67e7dedbc572791da164b04abc53
an82 <at82tong@gmail.com>
2018-10-19 15:08:43 -0400

    DBScan finding densest cluster and its center

--- dbscan_test.py
+++ dbscan_test.py
@@ -8,11 +8,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from collections import Counter
 import pandas as pd
+import statistics
 
 def get_centermost_point(cluster):
-    centroid = (MultiPoint(cluster).centroid.x, MultiPoint(cluster).centroid.y)
-    centermost_point = min(cluster, key=lambda point: great_circle(point, centroid).m)
-    return tuple(centermost_point)
+    lon_sum=0
+    lat_sum=0
+    for coor in cluster:
+        lon_sum = lon_sum+coor[0]
+        lat_sum = lat_sum+coor[1]
+    return (lon_sum/len(cluster),lat_sum/len(cluster))
 
 centers =np.array( [[40.430023, -86.909103], [40.422363, -86.876688], [40.422363, -86.876668], [40.425368, -86.895309], [40.366318, -86.752251]])
 X, labels_true = make_blobs(n_samples=750, centers=centers, cluster_std=0.3,
@@ -25,65 +29,25 @@ X = StandardScaler().fit_transform(X)
 eps=1.5/6371.0088
 db = DBSCAN(eps=eps, min_samples=1).fit(centers)
 print(db.labels_)
-#core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
-#core_samples_mask[db.core_sample_indices_] = True
+
 labels = db.labels_
 
 # Number of clusters in labels, ignoring noise if present.
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 c = Counter(labels)
 print(n_clusters_)
-largest_cluster = (c.most_common(1))[0][0]
-largest_cluster_index=[]
-for i in range(len(centers)):
-    if labels[i] == largest_cluster:
-        largest_cluster_index.append(centers[i])
+largest_cluster_index = (c.most_common(1))[0][0]
 
-print(largest_cluster_index)
-clusters = pd.Series([centers[largest_cluster]for n in range(n_clusters_)])
-print()
-print(get_centermost_point(clusters))
-#print(labels.most_common())
+largest_cluster = []
+for i in range(len(centers)):
+    if labels[i] == largest_cluster_index:
+        largest_cluster.append(centers[i].tolist())
+
+print(largest_cluster)
+
+print(get_centermost_point(largest_cluster))
 
 print('Estimated number of clusters: %d' % n_clusters_)
-#print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-#print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-#print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-#print("Adjusted Rand Index: %0.3f"
-#      % metrics.adjusted_rand_score(labels_true, labels))
-#print("Adjusted Mutual Information: %0.3f"
-#      % metrics.adjusted_mutual_info_score(labels_true, labels))
-#print("Silhouette Coefficient: %0.3f"
-#      % metrics.silhouette_score(X, labels))
 
-# #############################################################################
-# Plot result
 
-# Black removed and is used for noise instead.
-unique_labels = set(labels)
-colors = [plt.cm.Spectral(each)
-          for each in np.linspace(0, 1, len(unique_labels))]
-for k, col in zip(unique_labels, colors):
-    if k == -1:
-        # Black used for noise.
-        col = [0, 0, 0, 1]
-
-    class_member_mask = (labels == k)
-
-    xy=centers
-    plt.plot(xy, 'o', markerfacecolor=tuple(col),
-             markeredgecolor='k', markersize=14)
-    plt.plot(xy, 'o', markerfacecolor=tuple(col),
-             markeredgecolor='k', markersize=6)
-
-    # xy = X[class_member_mask & core_samples_mask]
-    # plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
-    #          markeredgecolor='k', markersize=14)
-    #
-    # xy = X[class_member_mask & ~core_samples_mask]
-    # plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
-    #          markeredgecolor='k', markersize=6)
-
-plt.title('Estimated number of clusters: %d' % n_clusters_)
-plt.show()
 
