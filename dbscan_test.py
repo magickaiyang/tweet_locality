e7ead881b724cc9376c546cd85d1c922c91ea3d2
an82 <at82tong@gmail.com>
2018-10-24 15:16:27 -0400

    Testing for get centermost function

--- dbscan_test.py
+++ dbscan_test.py
@@ -2,9 +2,15 @@ from math import sqrt
 from sklearn.cluster import DBSCAN
 import numpy as np
 from collections import Counter
-from sklearn.cluster import KMeans
 from shapely.geometry import MultiPoint
 from geopy.distance import great_circle
+import matplotlib.pyplot as plt
+
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn import metrics
+from sklearn.preprocessing import StandardScaler
+
+
 
 def get_centermost_point(cluster):
     centroid = (MultiPoint(cluster).centroid.x, MultiPoint(cluster).centroid.y)
@@ -21,8 +27,39 @@ def get_center_in_cluster(coordinates_list):
     db = DBSCAN(eps=eps, min_samples=1).fit(centers)
     labels = db.labels_
 
+    n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+    print("cluster number: "+str(n_clusters))
+
+
+    # Testing, plot the graph
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+
+    unique_labels = set(labels)
+    colors = [plt.cm.Spectral(each)
+                for each in np.linspace(0, 1, len(unique_labels))]
+
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            col = [0, 0, 0, 1]
+        class_member_mask = (labels == k)
+
+        xy = centers[class_member_mask & core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+                               markeredgecolor = 'k', markersize = 14)
+
+        xy = centers[class_member_mask & ~core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+                               markeredgecolor = 'k', markersize = 6)
+
+    plt.title('Estimated number of clusters: %d' % n_clusters)
+    plt.show()
+
+    # end of testing
+
+
     # get the cluster number
-    # n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
 
     # find the largest cluster
     c = Counter(labels)
@@ -71,9 +108,22 @@ def get_center_in_cluster(coordinates_list):
     # return pt
 
 
-result = get_center_in_cluster([[40.430023, -86.909103], [40.422363, -86.876788], [40.422363, -86.876683],
-                                [40.425368, -86.895309], [40.366318, -86.752251]])
-print result
+#result = get_center_in_cluster([[40.430023, -86.909103], [40.422363, -86.876788], [40.422363, -86.876683],
+#                                [40.425368, -86.895309], [40.366318, -86.752251]])
+#print result
+
+def test_cluster():
+    centers = [[40.422363, -86.876788], [40.422363, -86.876683]]
+    X, labels_true = make_blobs(n_samples = [8,6],centers=centers, cluster_std = 0.0004,
+                                random_state=0)
+    print X
+    print("======")
+    print(get_center_in_cluster(X))
 
 
 
+test_cluster()
+
+#result = get_center_in_cluster([[40.430023, -86.909103], [40.422363, -86.876788], [40.422363, -86.876683],
+#                                [40.425368, -86.895309], [40.366318, -86.752251]])
+#print result
