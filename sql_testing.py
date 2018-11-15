5ecfc9448b5dd412c68884ee6b4d0fc714b4ddaf
an82 <at82tong@gmail.com>
2018-11-15 14:28:22 -0500

    creating user table

--- sql_testing.py
+++ sql_testing.py
@@ -17,42 +17,53 @@ cnxn = pyodbc.connect(
     'DRIVER={SQL Server Native Client 10.0};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
 cursor = cnxn.cursor()
 
-cursor.execute("SELECT [created_at],[screen_name],[geo_lat],[geo_long],[tweet_text], [source] FROM [LOCALITY1].[dbo].[tweet_us_2016_01_03]")
+cursor.execute("SELECT [screen_name],[created_at],[geo_lat],[geo_long] FROM [LOCALITY1].[dbo].[tweet_us_2016_01]")
 row = cursor.fetchone()
 
 dicti = {}
+clusters = {}
 c = 0
 while row:
-    created_time = row[0]
-    username = row[1]
+    username = row[0]
+    created_time = str(row[1])
     coordinates = [row[2], row[3]]
-    text = row[4]
-    source = row[5]
+    # text = row[4]
     # if detectbot(username)<=2.5:
 
-    if username not in dicti.keys():
-        if ("Instagram" in source or "iPhone" in source or "Android" in source) and check_time(created_time):
-            dicti.update({username: {"coordinates": [coordinates], "text": [text], "home": []}})
+
+    # to find cluster for home location
+    if check_time(created_time):
+        if username not in clusters.keys():
+            clusters.update({username: {"coordinates": [coordinates], "home": []}})
         else:
-            dicti.update({username: {"coordinates": [], "text": [text], "home": []}})
-    else:
-        if ("Instagram" in source or "iPhone" in source or "Android" in source) and check_time(created_time):
-            dicti[username]["coordinates"].append(coordinates)
-            dicti[username]["text"].append(text)
-        else:
-            dicti[username]["text"].append(text)
+            clusters[username]["coordinates"].append(coordinates)
+
+
+    # cluster with text and coordinates
+    # if username not in dicti.keys():
+    #     if check_time(created_time):
+    #         dicti.update({username: {"coordinates": [coordinates], "text": [text], "home": []}})
+    #     else:
+    #         dicti.update({username: {"coordinates": [], "text": [text], "home": []}})
+    # else:
+    #     if check_time(created_time):
+    #         dicti[username]["coordinates"].append(coordinates)
+    #         dicti[username]["text"].append(text)
+    #     else:
+    #         dicti[username]["text"].append(text)
 
     row = cursor.fetchone()
 
+print(clusters)
 cursor = cnxn.cursor()
-for username in dicti.keys():
-    if len(dicti[username]["coordinates"]) > 20:
-        center = get_center_in_cluster(dicti[username]["coordinates"])
-        dicti[username]["home"] = center
-        print dicti[username]["home"]
+for username in clusters.keys():
+    if len(clusters[username]["coordinates"]) > 20:
+        center = get_center_in_cluster(clusters[username]["coordinates"])
+        clusters[username]["home"] = center
+        print clusters[username]["home"]
         # DO THIS FIRST
         execute_line = "INSERT INTO [LOCALITY1].[dbo].[twitter_users] (screenname, lat, lon) VALUES ('" + username + "', '" + str(
-            dicti[username]["home"][0]) + "', '" + str(dicti[username]["home"][1]) + "')"
+            clusters[username]["home"][0]) + "', '" + str(clusters[username]["home"][1]) + "')"
         # print(execute_line)
         cursor.execute(execute_line)
         cnxn.commit()
