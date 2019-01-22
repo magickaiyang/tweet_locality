ba9a2066913677fbde75a2cee067109369ca2b11
an82 <at82tong@gmail.com>
2019-01-22 16:36:24 -0500

    Scripts to export usertable along with the number of tweets into csv file, and the csv file

---
+++ usertable.py
@@ -0,0 +1,48 @@
+import csv
+import pyodbc
+import json
+
+
+def usertable_to_csv(data_table):
+    # Specify config
+    server = '128.46.137.201'
+    database = 'LOCALITY1'
+    username = 'localityedit'
+    password = 'Edit123'
+    # Connect to database
+    cnxn = pyodbc.connect(
+        'DRIVER={SQL Server Native Client 10.0};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    cnxn2 = pyodbc.connect(
+        'DRIVER={SQL Server Native Client 10.0};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    cursor = cnxn.cursor()
+    cursor2 = cnxn2.cursor()
+
+    # Write query and execute
+    query = "SELECT [users],[home_lat], [home_lon] FROM " + data_table
+    cursor.execute(query)
+
+    # Start with getting the first row
+    row = cursor.fetchone()
+    # row2 = cursor2.fetchone()
+
+    # Initialize the json data(username as key, coordinates as value)
+    data = {}
+    data['users'] = []
+
+    # Parsing each row
+    with open('usertable.csv', 'w') as f:
+        while row:
+            screenname = row[0]
+            lat = row[1]
+            lon = row[2]
+
+            num_tweets_query = "SELECT COUNT(*) FROM [LOCALITY1].[dbo].[tweets] where screen_name = '" + screenname + "'"
+            cursor2.execute(num_tweets_query)
+            row2 = cursor2.fetchone()
+            num_tweets = row2[0]
+
+            writer = csv.writer(f, lineterminator='\n', delimiter=',')
+            writer.writerow([float(lat), float(lon), num_tweets])
+            row = cursor.fetchone()
+
+usertable_to_csv("[LOCALITY1].[dbo].[twitter_users]")
