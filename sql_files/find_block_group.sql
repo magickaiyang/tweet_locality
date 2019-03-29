a304f826c51e5b2decf64b84515be697ba642714
Kaiyang Zhao <zh_kaiyang@hotmail.com>
2019-03-28 20:21:37 -0400

    fixed srid of blk_grp shapefile

--- sql_files/find_block_group.sql
+++ sql_files/find_block_group.sql
@@ -1,7 +1,6 @@
-SELECT users_2015_05_to_2017_12.user_id, us_blck_grp_2010.geoid10
-	FROM users_2015_05_to_2017_12
-	LEFT OUTER JOIN us_blck_grp_2010
-	ON ST_Intersects(us_blck_grp_2010.geom, ST_Transform(ST_SetSRID(ST_MakePoint(users_2015_05_to_2017_12.home_lon, users_2015_05_to_2017_12.home_lat),4326), 4269))
-	WHERE users_2015_05_to_2017_12.home_lon is not null
-	LIMIT 100
-	;
+UPDATE users_2014_10_to_2015_04 SET home_block_group =
+(
+	SELECT us_blck_grp_2010.geoid10 FROM us_blck_grp_2010
+		WHERE ST_Within(ST_Transform(ST_SetSRID(ST_MakePoint(users_2014_10_to_2015_04.home_lon, users_2014_10_to_2015_04.home_lat),4326), 102003), us_blck_grp_2010.geom)
+		LIMIT 1
+)
